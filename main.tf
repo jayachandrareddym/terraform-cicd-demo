b265903e1f8c5ceec4c40a6ba1bf8a6439b047c9
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2026-01-29 17:30:23 +0530

    Add demo security group resource
    Added a security group resource for demo purposes with ingress and egress rules.

--- main.tf
+++ main.tf
@@ -5,3 +5,24 @@ resource "random_id" "id" {
 resource "aws_s3_bucket" "demo" {
   bucket = "hashicorp-ug-demo-${random_id.id.hex}"
 }
+
+########### SG   #########
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo-open-sg"
+  description = "Insecure SG to be deleted after demo "
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
