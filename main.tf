3e00b02644ad76c88f4cb502ce7d8bb663a8f23f
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2026-01-29 18:38:40 +0530

    Comment out unused resources in main.tf
    Comment out random_id and aws_s3_bucket resources in main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "random_id" "id" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "demo" {
-  bucket = "hashicorp-ug-demo-${random_id.id.hex}"
-}
+#resource "random_id" "id" {
+#  byte_length = 4
+#}
+#
+#resource "aws_s3_bucket" "demo" {
+#  bucket = "hashicorp-ug-demo-${random_id.id.hex}"
+#}
 
 ########### SG   #########
 
