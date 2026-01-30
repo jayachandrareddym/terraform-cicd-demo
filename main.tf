590e15a9dab507dd3831e910f14cca9816643c8a
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2026-01-30 20:34:55 +0530

    Modify security group to allow all IPs on port 22
    Updated security group ingress rules to allow traffic from any IP.

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-#resource "random_id" "id" {
-#  byte_length = 4
-#}
-#
-#resource "aws_s3_bucket" "demo" {
-#  bucket = "hashicorp-ug-demo-${random_id.id.hex}"
-#}
-
-########### SG   #########
-
 resource "aws_security_group" "demo_sg" {
   name        = "demo-open-sg"
   description = "Insecure SG to be deleted after demo "
 
   ingress {
-    description = "allowed from my ip"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["223.181.108.36/32"]  
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 }
