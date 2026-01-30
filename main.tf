03696cea87f5451bd403eae1822731308487491a
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2026-01-30 21:51:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,16 +10,3 @@ resource "aws_security_group" "demo_sg" {
     cidr_blocks = ["223.181.108.36/32"]  
   }
 }
-
-resource "aws_security_group" "tf_demo_sg" {
-  name        = "hug-demo-sg"
-  description = "test sg with "
-
-  ingress {
-    description = "test-sg allowed from my ip" 
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["223.181.108.36/32"]  
-  }
-}
