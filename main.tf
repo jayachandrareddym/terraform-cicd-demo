89685d29b47a8aa50f83343a9ff5e90a42597ba9
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2026-01-31 12:17:00 +0530

    Create security group for CI/CD demo
    Added a new security group for Terraform CI/CD demo.

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ resource "aws_security_group" "demo_sg" {
     cidr_blocks = ["223.181.108.36/32"]  
   }
 }
+
+resource "aws_security_group" "tf_cicd_demo_sg" {
+  name        = "hug-demo-sg"
+  description = "creatig test sg for terraform ci/cd demo at hug meetup "
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+}
